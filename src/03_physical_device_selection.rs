d9cd82c581a072cf3641958e19fd5be1f13f1311
Brian Merchant <bhmerchant@gmail.com>
2018-09-17 08:46:04 -0700

    use structures to organize code

--- src/03_physical_device_selection.rs
+++ src/03_physical_device_selection.rs
@@ -8,51 +8,40 @@ extern crate gfx_backend_vulkan as back;
 extern crate gfx_hal as hal;
 extern crate winit;
 
-use hal::{Instance, QueueFamily};
+use hal::{queue, Adapter, Instance, QueueFamily};
+use winit::{dpi, ControlFlow, Event, EventsLoop, Window, WindowBuilder, WindowEvent};
 
 static WINDOW_NAME: &str = "03_physical_device_selection";
 
 fn main() {
     env_logger::init();
-    let (_window, events_loop) = init_window();
-    init_hal();
-    main_loop(events_loop);
-    clean_up();
+    let mut application = HelloTriangleApplication::init();
+    application.run();
+    application.clean_up();
 }
 
-fn find_queue_families(adapter: &hal::Adapter<back::Backend>) -> QueueFamilyIds {
-    let mut queue_family_ids = QueueFamilyIds::default();
-
-    for queue_family in &adapter.queue_families {
-        if queue_family.max_queues() > 0 && queue_family.supports_graphics() {
-            queue_family_ids.graphics_family = Some(queue_family.id());
-        }
-
-        if queue_family_ids.is_complete() {
-            break;
-        }
-    }
-
-    queue_family_ids
+struct WindowState {
+    events_loop: EventsLoop,
+    _window: Window,
 }
 
-fn is_adapter_suitable(adapter: &hal::Adapter<back::Backend>) -> bool {
-    find_queue_families(adapter).is_complete()
+struct HalState {
+    _adapter: Adapter<back::Backend>,
+    _instance: back::Instance,
 }
 
-fn pick_adapter(instance: &back::Instance) -> hal::Adapter<back::Backend> {
-    let adapters = instance.enumerate_adapters();
-    for adapter in adapters {
-        if is_adapter_suitable(&adapter) {
-            return adapter;
-        }
-    }
-    panic!("No suitable adapter");
+impl HalState {
+    fn clean_up(self) {}
+}
+
+struct HelloTriangleApplication {
+    hal_state: HalState,
+    window_state: WindowState,
 }
 
 #[derive(Default)]
 struct QueueFamilyIds {
-    graphics_family: Option<hal::queue::QueueFamilyId>,
+    graphics_family: Option<queue::QueueFamilyId>,
 }
 
 impl QueueFamilyIds {
@@ -61,32 +50,90 @@ impl QueueFamilyIds {
     }
 }
 
-fn init_window() -> (winit::Window, winit::EventsLoop) {
-    let events_loop = winit::EventsLoop::new();
-    let window_builder = winit::WindowBuilder::new()
-        .with_dimensions(winit::dpi::LogicalSize::new(1024., 768.))
-        .with_title(WINDOW_NAME.to_string());
-    let window = window_builder.build(&events_loop).unwrap();
-    (window, events_loop)
+impl HelloTriangleApplication {
+    pub fn init() -> HelloTriangleApplication {
+        let window_state = HelloTriangleApplication::init_window();
+        let hal_state = HelloTriangleApplication::init_hal();
+
+        HelloTriangleApplication {
+            hal_state,
+            window_state,
+        }
+    }
+
+    fn init_window() -> WindowState {
+        let events_loop = EventsLoop::new();
+        let window_builder = WindowBuilder::new()
+            .with_dimensions(dpi::LogicalSize::new(1024., 768.))
+            .with_title(WINDOW_NAME.to_string());
+        let window = window_builder.build(&events_loop).unwrap();
+        WindowState {
+            events_loop,
+            _window: window,
+        }
+    }
+
+    fn init_hal() -> HalState {
+        let instance = HelloTriangleApplication::create_instance();
+        let adapter = HelloTriangleApplication::pick_adapter(&instance);
+        HalState {
+            _adapter: adapter,
+            _instance: instance,
+        }
+    }
+
+    fn create_instance() -> back::Instance {
+        back::Instance::create(WINDOW_NAME, 1)
+    }
+
+    fn find_queue_families(adapter: &Adapter<back::Backend>) -> QueueFamilyIds {
+        let mut queue_family_ids = QueueFamilyIds::default();
+
+        for queue_family in &adapter.queue_families {
+            if queue_family.max_queues() > 0 && queue_family.supports_graphics() {
+                queue_family_ids.graphics_family = Some(queue_family.id());
+            }
+
+            if queue_family_ids.is_complete() {
+                break;
+            }
+        }
+
+        queue_family_ids
+    }
+
+    fn is_adapter_suitable(adapter: &Adapter<back::Backend>) -> bool {
+        HelloTriangleApplication::find_queue_families(adapter).is_complete()
+    }
+
+    fn pick_adapter(instance: &back::Instance) -> Adapter<back::Backend> {
+        let adapters = instance.enumerate_adapters();
+        for adapter in adapters {
+            if HelloTriangleApplication::is_adapter_suitable(&adapter) {
+                return adapter;
+            }
+        }
+        panic!("No suitable adapter");
+    }
+
+    fn main_loop(&mut self) {
+        self.window_state
+            .events_loop
+            .run_forever(|event| match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => ControlFlow::Break,
+                _ => ControlFlow::Continue,
+            });
+    }
+
+    fn run(&mut self) {
+        self.main_loop();
+    }
+
+    fn clean_up(self) {
+        self.hal_state.clean_up();
+    }
 }
 
-fn create_instance() -> back::Instance {
-    back::Instance::create(WINDOW_NAME, 1)
-}
-
-fn init_hal() {
-    let instance = create_instance();
-    let _adapter = pick_adapter(&instance);
-}
-
-fn clean_up() {}
-
-fn main_loop(mut events_loop: winit::EventsLoop) {
-    events_loop.run_forever(|event| match event {
-        winit::Event::WindowEvent {
-            event: winit::WindowEvent::CloseRequested,
-            ..
-        } => winit::ControlFlow::Break,
-        _ => winit::ControlFlow::Continue,
-    });
-}
